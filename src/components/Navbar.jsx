47746a5a3013ec291e31e17a429a7ad97a3a9b13
Rahul Sharma <therahulsharma15@gmail.com>
2025-11-16 11:21:56 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -41,7 +41,7 @@ export default function Navbar() {
             isScrolled ? "text-yellow-600" : "text-yellow-400"
           }`}
         >
-          Shree Shyam  Ceilings
+          Shree Shyam Fall Ceilings
         </Link>
 
         {/* Desktop Menu */}
